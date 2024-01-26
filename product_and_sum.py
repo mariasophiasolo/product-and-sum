6c1282745a4bea566adccdddc39c9725624421aa
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-26 15:20:21 +0800

    working

--- product_and_sum.py
+++ product_and_sum.py
@@ -1,3 +1,23 @@
 # calculating the product and sum of two numbers
 
+def product_or_sum (number1,number2):
+    product = number1 * number2
+
+    if product <= 1000:
+        return product
+    else: 
+        return number1 + number2
+
+numbergiven1 = 20
+numbergiven2 = 30 
+
+result = product_or_sum (numbergiven1,numbergiven2)
+print ("The result is: ", result)
+
+# calculate the product of the two numbers
+# check if the product is less than 1000
+# if the product is greater than 1000 then calculate for its sum
+
+# first given 
+# second given
 
